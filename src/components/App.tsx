d7f4f52f58ff3bc74abb0e2902fc348238e664ed
Brian Ingles <github@emeraldwalk.com>
2020-01-25 07:26:21 -0600

    Added comments and renamed some functions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
-import { calculateElapsedSeconds, getDisplayText, State, toggle } from '../state';
+import {
+  addTimeEntry,
+  calculateElapsedSeconds,
+  buttonDisplayText,
+  State
+} from '../state';
 
 function* reverse<T>(
   array: T[],
@@ -13,30 +18,28 @@ export interface AppProps {
 }
 
 const App: React.FC<AppProps> = () => {
+  // track requestAnimationFrame so we can cancel it on unmount
+  const animationFrameHandleRef = useRef<
+    ReturnType<typeof requestAnimationFrame>
+  >()
+
   const [elapsed, setElapsed] = useState()
+  const [label, setLabel] = useState('')
 
   const stateRef = useRef<State>({
     isRunning: false,
     entries: [],
   })
 
-  // const [displayText, setDisplayText] = useState(
-  //   () => getDisplayText(
-  //     stateRef.current,
-  //   )
-  // )
-
-  const [label, setLabel] = useState('')
-
-  const animationFrameHandleRef = useRef<ReturnType<typeof requestAnimationFrame>>()
-  const timeRef = useRef<number>()
-
+  /**
+   * Animation loop sets elapsed seconds so that we get
+   * a re-render whenever it changes
+   */
   function animate(time: number) {
     setElapsed(
       calculateElapsedSeconds(stateRef.current),
     )
 
-    timeRef.current = time
     animationFrameHandleRef.current = requestAnimationFrame(animate)
   }
 
@@ -51,7 +54,7 @@ const App: React.FC<AppProps> = () => {
 
   const onClick = useCallback(
     () => {
-      stateRef.current = toggle(
+      stateRef.current = addTimeEntry(
         stateRef.current,
         label,
       )
@@ -59,7 +62,7 @@ const App: React.FC<AppProps> = () => {
     [label],
   )
 
-  const { type = 'stop' } = stateRef.current.entries[0] || {}
+  const [{ type } = { type: 'stop' }] = stateRef.current.entries
 
   return (
     <div className="c_app">
@@ -74,7 +77,7 @@ const App: React.FC<AppProps> = () => {
       <button
         className={`c_app__cmd ${type}`}
         onClick={onClick}
-      >{getDisplayText(stateRef.current, elapsed)}
+      >{buttonDisplayText(stateRef.current, elapsed)}
       </button>
 
       <ul>
--- src/state.ts
+++ src/state.ts
@@ -9,47 +9,64 @@ export interface State {
   entries: TimeEntry[],
 }
 
-export function calculateElapsedSeconds(
-  { entries: [firstEntry] }: State
-): number | null {
-  if (!firstEntry) {
-    return null
-  }
-
-  return Math.floor(
-    (Date.now() - firstEntry?.time) / 1000
-  )
-}
-
-export function displayTime(
-  ms: number,
-): string {
-  return new Date(ms).toISOString().substr(11, 8)
-}
-
-export function isRunning(
-  state: State,
-): boolean {
-  return state.entries[0]?.type === 'start'
-}
-
-export function getDisplayText(
-  { entries: [firstEntry] }: State,
+/**
+ * Display text for primary button
+ */
+export function buttonDisplayText(
+  { entries: [lastEntry] }: State,
   elapsedSeconds: number,
 ): string {
 
-  if (!firstEntry || firstEntry.type === 'stop') {
+  if (!lastEntry || lastEntry.type === 'stop') {
     return 'Start'
   }
 
   return displayTime(elapsedSeconds * 1000)
 }
 
-export function toggle(
+/**
+ * Calculate elapsed time in seconds since
+ * last entry
+ */
+export function calculateElapsedSeconds(
+  { entries: [lastEntry] }: State
+): number | null {
+  if (!lastEntry) {
+    return null
+  }
+
+  return Math.floor(
+    (Date.now() - lastEntry?.time) / 1000
+  )
+}
+
+/**
+ * Display milliseconds as time in seconds
+ * 00:00:00
+ * @param ms
+ */
+export function displayTime(
+  ms: number,
+): string {
+  return new Date(ms).toISOString().substr(11, 8)
+}
+
+/**
+ * Is the timer running
+ */
+export function isRunning(
+  { entries: [lastEntry] }: State,
+): boolean {
+  return lastEntry?.type === 'start'
+}
+
+/**
+ * Add a new time entry with given label
+ */
+export function addTimeEntry(
   state: State,
   label: string,
 ): State {
-  console.log({ label })
   return {
     ...state,
     entries: [
